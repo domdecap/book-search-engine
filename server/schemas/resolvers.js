cca4a9dab866629862e101e24cba8bece1bc51e0
dom <dominic.decapite@gmail.com>
2024-07-30 22:00:32 -0400

    Created schema files. Deleted routes folder.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+
+    Mutations: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect login');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect login');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You must be logged in to do this');
+
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You must be logged in to do this')
+        },
+
+
+    },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+type User {
+_id: ID
+username: String
+email: String
+bookCount: Int
+savedBooks: [Book]
+}
+
+type Book {
+bookId: ID
+description: String
+title: String
+image: String
+link: String
+authors: [String]
+
+}
+
+type Auth {
+token: ID
+user: User
+
+}
+
+input BookInput {
+authors: [String]
+description: String
+bookId: String
+image: String
+link: String
+title: String
+}
+
+type Query {
+me: User
+
+}
+
+type Mutation {
+addUser(username: String, email: String, password: String): Auth
+login(email: String, password: String): Auth
+addBook(bookData: BookInput): User
+removeBook(bookId: ID): User
+}
+`
+module.exports = typeDefs;
