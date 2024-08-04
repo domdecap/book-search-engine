736e67d3ccb70e7db038876c68c91b8c4cc0a392
dom <dominic.decapite@gmail.com>
2024-08-03 21:02:19 -0400

    Fix signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: 'http://localhost:3002/graphql',
 });
 
 
